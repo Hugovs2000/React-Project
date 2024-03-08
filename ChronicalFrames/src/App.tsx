4e08eb6a7c21eea51793821ed49a9fd1fdf72252
Hugo van Schalkwyk <hugovans2000@gmail.com>
2024-03-08 10:28:48 +0200

    feat: :sparkles: Setup React project aka ChronicalFrames
    Set up the project by installing tailwind as well as daisyUI and set up the base structure for mobile view

---
+++ ChronicalFrames/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './Footer';
+import MainContent from './MainContent';
+import Navbar from './Navbar';
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <MainContent></MainContent>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
---
+++ ChronicalFrames/src/MainContent.tsx
@@ -0,0 +1,9 @@
+function MainContent(){
+    return (
+        <>
+        <div className=" bg-zinc-800 h-full"></div>
+        </>
+    )
+}
+
+export default MainContent;
