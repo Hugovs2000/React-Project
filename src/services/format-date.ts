8391dd704d06dffe0dfae4ead9029ebc3621b5cc
Hugo van Schalkwyk <hugovans2000@gmail.com>
2024-03-15 09:09:55 +0200

    fix: :bug: Removed Comments

--- src/services/format-date.ts
+++ src/services/format-date.ts
@@ -1,14 +1,10 @@
 import { formatDistanceToNow } from "date-fns";
 
 export default function formatDateToTime(date: string) {
-  // Your ISO date string
   const isoDateString = date;
 
-  // Convert ISO string to Date object
   const newdate = new Date(isoDateString);
 
-  // Format the date
-  // You can adjust the format as needed
   const formattedTime = newdate.toLocaleString("en-US", {
     year: "numeric",
     month: "2-digit",
