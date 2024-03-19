042ff8505f7f9a0beee852ea9bd0e5c2d3526806
Hugo van Schalkwyk <hugovans2000@gmail.com>
2024-03-19 19:06:23 +0200

    fix: :bug: Improved skeletons by making use of DaisyUI Skeleton

--- src/modules/HomePage/components/Skeletons/LatestUpdatesSkeleton.tsx
+++ src/modules/HomePage/components/Skeletons/LatestUpdatesSkeleton.tsx
@@ -1,22 +1,22 @@
 export default function LatestUpdatesSkeleton() {
   return (
     <div className="my-8 px-8 flex flex-col items-center">
-      <div className="self-start rounded-lg w-1/2 h-10 text-xl bg-gradient-to-r from-gray-700 to-gray-600 text-center"></div>
-      <div className="flex flex-nowrap py-8 gap-8 md:gap-16 overflow-x-scroll max-w-full">
-        <div className="indicator rounded-xl min-w-40 h-52 bg-gradient-to-r from-gray-700 to-gray-600">
-          <span className="indicator-item translate-x-8 badge py-3  bg-gradient-to-r from-gray-700 to-gray-600 z-20 mx-2 w-10 border-none"></span>
+      <div className="skeleton bg-zinc-900 self-start rounded-lg w-1/2 h-10 text-xl  text-center"></div>
+      <div className="flex flex-nowrap py-8 gap-8 md:gap-16 overflow-x-hidden w-full">
+        <div className="skeleton bg-zinc-900 indicator rounded-xl min-w-40 h-52">
+          <span className="skeleton bg-zinc-900 indicator-item translate-x-8 badge py-3 z-20 mx-2 w-10 border-none"></span>
         </div>
-        <div className="indicator rounded-xl min-w-40 h-52 bg-gradient-to-r from-gray-700 to-gray-600">
-          <span className="indicator-item translate-x-8 badge py-3  bg-gradient-to-r from-gray-700 to-gray-600 z-20 mx-2 w-10 border-none"></span>
+        <div className="skeleton bg-zinc-900 indicator rounded-xl min-w-40 h-52 ">
+          <span className="skeleton bg-zinc-900 indicator-item translate-x-8 badge py-3 z-20 mx-2 w-10 border-none"></span>
         </div>
-        <div className="indicator rounded-xl min-w-40 h-52 bg-gradient-to-r from-gray-700 to-gray-600">
-          <span className="indicator-item translate-x-8 badge py-3  bg-gradient-to-r from-gray-700 to-gray-600 z-20 mx-2 w-10 border-none"></span>
+        <div className="skeleton bg-zinc-900 indicator rounded-xl min-w-40 h-52">
+          <span className="skeleton bg-zinc-900 indicator-item translate-x-8 badge py-3 z-20 mx-2 w-10 border-none"></span>
         </div>
-        <div className="indicator rounded-xl min-w-40 h-52 bg-gradient-to-r from-gray-700 to-gray-600">
-          <span className="indicator-item translate-x-8 badge py-3  bg-gradient-to-r from-gray-700 to-gray-600 z-20 mx-2 w-10 border-none"></span>
+        <div className="skeleton bg-zinc-900 indicator rounded-xl min-w-40 h-52">
+          <span className="skeleton bg-zinc-900 indicator-item translate-x-8 badge py-3 z-20 mx-2 w-10 border-none"></span>
         </div>
-        <div className="indicator rounded-xl min-w-40 h-52 bg-gradient-to-r from-gray-700 to-gray-600">
-          <span className="indicator-item translate-x-8 badge py-3  bg-gradient-to-r from-gray-700 to-gray-600 z-20 mx-2 w-10 border-none"></span>
+        <div className="skeleton bg-zinc-900 indicator rounded-xl min-w-40 h-52">
+          <span className="skeleton bg-zinc-900 indicator-item translate-x-8 badge py-3 z-20 mx-2 w-10 border-none"></span>
         </div>
       </div>
     </div>
--- src/modules/ReadMangaPage/components/Skeletons/ReadPageSkeleton.tsx
+++ src/modules/ReadMangaPage/components/Skeletons/ReadPageSkeleton.tsx
@@ -3,23 +3,17 @@ import { RiArrowGoBackLine } from "react-icons/ri";
 
 export default function ReadPageSkeleton({ manga }: { manga: string }) {
   return (
-    <>
-      <div className="h-full bg-gradient-to-t from-gray-900 to-gray-800 flex flex-col">
-        <div className="z-[1] h-10 fixed top-16 p-2 w-full flex justify-center gap-8 items-center bg-gradient-to-r from-gray-700 to-gray-600 rounded-b-md">
-          <div className="absolute left-0 mx-4 h-fit">
-            <Link
-              to="/details/$manga"
-              params={{
-                manga: manga,
-              }}
-              className="h-full text-slate-50">
-              <RiArrowGoBackLine className="scale-125" />
-            </Link>
-          </div>
-          <div className="text-lg font-extrabold text-slate-50"></div>
-        </div>
+    <div className="skeleton rounded-none h-full flex flex-col relative bg-zinc-900">
+      <div className="absolute left-0 m-4 h-fit">
+        <Link
+          to="/details/$manga"
+          params={{
+            manga: manga,
+          }}
+          className="h-full text-slate-50">
+          <RiArrowGoBackLine className="scale-125" />
+        </Link>
       </div>
-      <div className="z-[1] h-10 fixed bottom-0 p-2 w-full flex gap-10 md:gap-28 items-center bg-gradient-to-r from-gray-700 to-gray-600 rounded-t-md"></div>
-    </>
+    </div>
   );
 }
