d92f8e1eb5cb9caa51839a966edd0601505a107f
Hugo van Schalkwyk <hugovans2000@gmail.com>
2024-03-27 08:10:46 +0200

    fix: :bug: Fixed merge mistake in the DrawerSeasonalSection
    There was duplicate code from the merge

--- src/modules/Drawer/components/DrawerSeasonalSection.tsx
+++ src/modules/Drawer/components/DrawerSeasonalSection.tsx
@@ -12,29 +12,6 @@ export default function DrawerTrendingSection({
 
   return (
     <>
-      <h2 className="font-bold">{resultingSeasonString} Manhwa</h2>
-      {seasonalData.data
-        ?.filter(
-          (item) =>
-            item.content_rating === "safe" && !!item.md_covers?.[0]?.b2key
-        )
-        .slice(0, 5)
-        .map(
-          (comic) =>
-            comic?.slug &&
-            comic?.title && (
-              <Link
-                to="/details/$manga"
-                params={{
-                  manga: comic.slug,
-                }}
-                key={comic.slug}>
-                <li className="text-slate-50 ml-4 my-1 text-nowrap max-w-full text-ellipsis overflow-hidden text-sm">
-                  {comic.title}
-                </li>
-              </Link>
-            )
-        )}
       <h2 className="font-bold text-emerald-600 mt-4 text-lg">
         {resultingSeasonString} Manwha
       </h2>
