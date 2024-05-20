be79fa1b129c443d2c32ebec4367cf572dacf1e8
Hugo van Schalkwyk <hugovans2000@gmail.com>
2024-05-20 13:11:39 +0200

    feat: ✨ Implemented PWA configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,47 @@
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+  manifest: {
+    name: 'Chronical Frames',
+    short_name: 'Chronical-Frames',
+    description: 'Manga Reader Web App',
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png'
+      },
+      {
+        src: '/maskable_icon.png',
+        sizes: '48x48',
+        type: 'image/png',
+        purpose: 'any maskable'
+      }
+    ],
+    theme_color: '#059669',
+    background_color: '#f0e7db',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait'
+  }
+};
+
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), VitePWA(manifestForPlugin), TanStackRouterVite()],
 });
