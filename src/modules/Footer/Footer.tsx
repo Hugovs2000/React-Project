7e7f47ef6a4f6271e9b1f71dc2af41a3828141f8
Hugo van Schalkwyk <hugovans2000@gmail.com>
2024-05-27 10:35:54 +0200

    style: 💄 Removed copyright text from footer

--- src/modules/Footer/Footer.tsx
+++ src/modules/Footer/Footer.tsx
@@ -1,6 +1,5 @@
 import { IoLogoGithub } from "react-icons/io";
 import { IoLogoLinkedin } from "react-icons/io5";
-import { RiPagesLine } from "react-icons/ri";
 
 export default function Footer({ padding }: { padding: string }) {
   return (
@@ -10,17 +9,15 @@ export default function Footer({ padding }: { padding: string }) {
         className={`footer items-center gap-4 bg-zinc-900 text-slate-50 ${padding}`}
       >
         <aside className="grid-flow-col items-center">
-          <RiPagesLine className="size-10" />
-          <p>Copyright © 2024 - All rights reserved</p>
+          <nav className="flex grid-flow-col items-center gap-4 md:place-self-center md:justify-self-end">
+            <a href="https://github.com/Hugovs2000/React-Project.git">
+              <IoLogoGithub className="size-8" />
+            </a>
+            <a href="https://www.linkedin.com/in/hugo-van-schalkwyk-312180248/">
+              <IoLogoLinkedin className="size-8" />
+            </a>
+          </nav>
         </aside>
-        <nav className="flex grid-flow-col items-center gap-4 md:place-self-center md:justify-self-end">
-          <a href="https://github.com/Hugovs2000/React-Project.git">
-            <IoLogoGithub className="size-8" />
-          </a>
-          <a href="https://www.linkedin.com/in/hugo-van-schalkwyk-312180248/">
-            <IoLogoLinkedin className="size-8" />
-          </a>
-        </nav>
       </footer>
     </>
   );
